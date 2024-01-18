684abce456fda42957281cb3addb8922510aaf59
Anton Zelenin <ntnzelenin@gmail.com>
2024-01-18 12:17:50 +0200

    Add length constraints to email and password in serializers

--- webapp/serializers/registration.py
+++ webapp/serializers/registration.py
@@ -2,21 +2,21 @@ from rest_framework import serializers
 
 
 class EmailSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.EmailField(max_length=254)
 
 
 class EmailVerificationCodeSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.EmailField(max_length=254)
     verification_code = serializers.CharField()
 
 
 class EmailPasswordSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+    email = serializers.EmailField(max_length=254)
+    password = serializers.CharField(max_length=128)
     create_password_token = serializers.CharField()
 
 
 class EmailUsernameSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    username = serializers.CharField()
+    email = serializers.EmailField(max_length=254)
+    username = serializers.CharField(max_length=150)
     create_username_token = serializers.CharField()
